5421994ac47f9002e7855b8810129160b56e7052
mdenitti <massimodn77@gmail.com>
2025-05-20 19:25:36 +0200

    Add deployment workflow and post-deployment script

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+phpinfo();
+// This is a simple PHP script that outputs the PHP configuration information
+// The phpinfo() function outputs a large amount of information about the current state of PHP
